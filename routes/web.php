45e00e8fda97fd9deb3c8fefabf723aa15d3b28b
Alejandro Pérez Durán <apduran95@gmail.com>
2025-03-21 20:47:57 -0400

    Add about page, update routes, and modify welcome message

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('/about', 'about');
+
+Route::get("/product/{id?}", function (int $id = null) {
+    return "<h1>Product ID: {$id}</h1>";
+})->whereNumber('id');
+
+Route::get('/user/{name}', function (string $name) {
+    return "<h1>User: {$name}</h1>";
+})->whereAlpha('name');
